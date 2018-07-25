a6f2f71efd7b10465d4c1704f4bcc99245ca9d5c
Jose Moreno <t-jomor@microsoft.com>
2018-07-25 13:41:28 -0700

    take2

--- examples/infinity_hashtags_liker.py
+++ examples/infinity_hashtags_liker.py
@@ -17,13 +17,14 @@ parser = argparse.ArgumentParser(add_help=True)
 parser.add_argument('-u', type=str, help="username")
 parser.add_argument('-p', type=str, help="password")
 parser.add_argument('-proxy', type=str, help="proxy")
-parser.add_argument('hashtags', type=str, nargs='+', help='hashtags')
+#parser.add_argument('hashtags', type=str, nargs='+', help='hashtags')
 args = parser.parse_args()
 
 bot = Bot()
 bot.login(username=args.u, password=args.p,
           proxy=args.proxy)
 
+#get hashtags from text file instead of args
 like_hashtags_list = bot.read_list_from_file("like_hashtags.txt")
 
 while True:
