c7b350b1cc63824eb46e046ef6161ae98ff1ff39
Jose Moreno <moreno19@purdue.edul>
2018-08-10 20:59:19 -0700

    roundrobin commenting

--- examples/comment/master_comment.py
+++ examples/comment/master_comment.py
@@ -1,9 +1,48 @@
 import os
 import subprocess
 
-subprocess.call(["python","comment_hashtags.py","turtlecomments.txt", "turtle", "savetheturtles"])
-subprocess.call(["python","comment_hashtags.py","bobacomments.txt", "boba", "bubbletea"])
-subprocess.call(["python","comment_hashtags.py","smoothiecomments.txt", "vegansmoothie", "smoothie"])
-subprocess.call(["python","comment_hashtags.py","strawcomments.txt", "strawssuck", "plasticpollutes"])
-subprocess.call(["python","comment_hashtags.py","beachresortcomments.txt", "beachcleanup", "ecoresort"])
-subprocess.call(["python","comment_hashtags.py","teacomments.txt", "tea", "yerbamate", "tealeaves"])
+def switchup():
+    f = open("choice.txt", "r")
+    num = int(f.read()[0])
+    f.close()
+
+    f = open("choice.txt", "w")
+    if num is not 6:
+        num += 1
+        f.write(str(num))
+    else:
+        num = 0
+        f.write(str(num))
+    f.close()
+    return num
+
+
+f = open("choice.txt", "r")
+choice = int(f.read()[0])
+
+while(1):
+    if choice is 0:
+        choice = switchup()
+        subprocess.call(["python","comment_hashtags.py","turtlecomments.txt", "turtle", "savetheturtles"])
+
+    if choice is 1:
+        choice = switchup()
+        subprocess.call(["python","comment_hashtags.py","bobacomments.txt", "boba", "bubbletea"])
+
+    if choice is 2:
+        choice = switchup()
+        subprocess.call(["python","comment_hashtags.py","smoothiecomments.txt", "vegansmoothie", "smoothie"])
+
+    if choice is 3:
+        choice = switchup()
+        subprocess.call(["python","comment_hashtags.py","strawcomments.txt", "strawssuck", "plasticpollutes"])
+
+    if choice is 4:
+        choice = switchup()
+        subprocess.call(["python","comment_hashtags.py","beachresortcomments.txt", "beachcleanup", "ecoresort"])
+
+    if choice is 5:
+        choice = switchup()
+        subprocess.call(["python","comment_hashtags.py","teacomments.txt", "tea", "yerbamate", "tealeaves"])
+    if choice is 6:
+        choice = switchup()
